e4c77bf097500c70fd1f63225f7810961634e38f
Jesús de Villar <jesusdevillar@macbook-pro-de-jesus.local>
2016-04-13 22:30:10 +0200

    Initial Commit

---
+++ PizzaWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzaWatch
+//
+//  Created by Jesús de Villar on 13/4/16.
+//  Copyright © 2016 JdeVillar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
